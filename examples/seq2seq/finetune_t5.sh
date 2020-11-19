f856ce70ea138dc51b46f900a50cabb6e60d89fd
Jona Löffler <jona.loeffler@online.de>
2020-11-19 17:51:26 +0100

    Ajust t5 finetuning parameters

--- examples/seq2seq/finetune_t5.sh
+++ examples/seq2seq/finetune_t5.sh
@@ -2,13 +2,13 @@
 export PYTHONPATH="../":"${PYTHONPATH}"
 
 python finetune.py \
---data_dir=$CNN_DIR \
+--data_dir=./train_data \
 --learning_rate=3e-5 \
---train_batch_size=$BS \
---eval_batch_size=$BS \
---output_dir=$OUTPUT_DIR \
+--train_batch_size=4 \
+--eval_batch_size=4 \
+--output_dir=./output \
 --max_source_length=512 \
---max_target_length=56 \
 --val_check_interval=0.1 --n_val=200 \
 --do_train --do_predict \
+--gpus=0 \
  "$@"
