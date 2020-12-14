60b618315cf030123ab5d79ec6e1e2e5b9a9b195
Jona Löffler <jonaloeffler@uni-koblenz.de>
2020-12-14 15:26:30 +0000

    Remove preset directory parameters

--- examples/seq2seq/finetune_t5.sh
+++ examples/seq2seq/finetune_t5.sh
@@ -2,11 +2,9 @@
 export PYTHONPATH="../":"${PYTHONPATH}"
 
 python finetune.py \
---data_dir=./train_data \
 --learning_rate=3e-5 \
 --train_batch_size=4 \
 --eval_batch_size=4 \
---output_dir=./output \
 --max_source_length=512 \
 --val_check_interval=0.1 --n_val=200 \
 --do_train --do_predict \
